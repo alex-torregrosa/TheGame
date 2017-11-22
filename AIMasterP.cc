62dd98439a457b05bf0b92a09b36cb0f2e2aca1b
Alex Torregrosa <alex@iMac.local>
2017-11-23 00:30:24 +0100

    Dijkstra mola

--- AIMasterP.cc
+++ AIMasterP.cc
@@ -11,27 +11,34 @@ struct PLAYER_NAME : public Player {
    * Factory: returns a new instance of this class.
    * Do not modify this function.
    */
-  static Player *factory() { return new PLAYER_NAME; }
+  static Player* factory() { return new PLAYER_NAME; }
 
   /**
    * Types and attributes for your player can be defined here.
    */
-
+  // int types
   typedef vector<int> intV;
-  typedef map<int, int> intMap;
-
   typedef vector<intV> intMat;
+  typedef map<int, int> intMap;
+  // bool types
   typedef vector<bool> boolV;
   typedef vector<boolV> boolMat;
+  // Pos types
   typedef queue<Pos> PosQ;
   typedef vector<Pos> posV;
-  /* typedef struct vPos {
-    Pos m, p;
-    vPos(Pos me) : m(me), p(Pos(-1, -1)) {}
-    vPos(Pos me, Pos par) : m(me), p(par) {}
-  }; */
   typedef map<Pos, Pos> posMap;
 
+  // Position + distance
+  struct dPos {
+    Pos p;
+    int d;
+    dPos(Pos pos, int dist) : p(pos), d(dist) {}
+    bool operator>(const dPos& other) const { return d > other.d; }
+  };
+  typedef priority_queue<dPos, vector<dPos>, greater<dPos>>
+      PosPQ;  // TODO Maybe change Container??
+
+  // Valid ork states
   enum orkStates { SEARCH_C, SEARCH_P, KEEP, KILL, ORK_STATES_SIZE };
 
   // Controller variables:
@@ -39,7 +46,7 @@ struct PLAYER_NAME : public Player {
   intMap status;
 
   // Devuelve las casillas vecinas a la que es posible moverse
-  void veins(Pos pos, posV &res) {
+  void veins(Pos pos, posV& res) {
     for (int d = 0; d != DIR_SIZE; ++d) {
       Dir dir = Dir(d);
       Pos npos = pos + dir;
@@ -66,7 +73,6 @@ struct PLAYER_NAME : public Player {
     posMap parents;
     visited[pos.i][pos.j] = true;
     q.push(pos);
-    posV neig;
 
     Pos p;
     bool found = false;
@@ -77,7 +83,7 @@ struct PLAYER_NAME : public Player {
         found = true;
       else {
         q.pop();
-        neig.clear();
+        posV neig;
         veins(p, neig);
         for (Pos n : neig) {
           if (not visited[n.i][n.j]) {
@@ -103,6 +109,55 @@ struct PLAYER_NAME : public Player {
     _unreachable();
   }
 
+  Dir dijkstra(Pos pos, CellType ct) {
+    PosPQ pq;
+    intMat prices(rows(), intV(cols(), -1));
+    posMap parents;
+    prices[pos.i][pos.j] = 0;
+    pq.push(dPos(pos, 0));
+
+    dPos dp =
+        pq.top();  // Initialized to valid value, will be overwritten later
+    bool found = false;
+
+    while (not found and not pq.empty()) {
+      dp = pq.top();
+      Cell c = cell(dp.p);
+      if (cmp_search(ct, c))
+        found = true;
+      else {
+        pq.pop();
+
+        posV neig;
+        veins(dp.p, neig);
+        for (Pos n : neig) {
+          int newd = dp.d + 1 + cost(cell(n.i, n.j).type);
+          if (prices[n.i][n.j] == -1 or newd < prices[n.i][n.j]) {
+            pq.push(dPos(n, newd));
+            prices[n.i][n.j] = newd;
+            parents[n] = dp.p;
+          }
+        }
+      }
+    }
+    Pos p = dp.p;
+
+    while (p != pos and parents[p] != pos) p = parents[p];
+
+    if (p == pos) return Dir(NONE);
+
+    // Calculate Initial direction
+    for (int d = 0; d != DIR_SIZE; ++d) {
+      Dir dir = Dir(d);
+      Pos npos = pos + dir;
+      if (npos == p) {
+        return dir;
+      }
+    }
+
+    _unreachable();
+  }
+
   /**
    * Moves the player
    */
@@ -110,8 +165,8 @@ struct PLAYER_NAME : public Player {
     Unit u = unit(id);
     int s = status[id];
     Dir d;
-    if (s == SEARCH_C) d = bfs(u.pos, CITY);
-    if (s == SEARCH_P) d = bfs(u.pos, PATH);
+    if (s == SEARCH_C) d = dijkstra(u.pos, CITY);
+    if (s == SEARCH_P) d = dijkstra(u.pos, PATH);
     execute(Command(id, d));
   }
 
--- runGame.sh
+++ runGame.sh
@@ -1,5 +1,5 @@
 
 
-./Game MasterP MasterP Dummy Dummy -s $0 -i default.cnf -o default.out;
+./Game MasterP Dummy Dummy Dummy -s $0 -i default.cnf -o default.out;
 
 cp default.out Viewer/default.out
